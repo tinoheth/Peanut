dab31da97de7aa1f6058aab419065b31422429c8
Tino Heth <th@t-no.de>
2014-11-11 15:59:17 +0100

    Phasing out list

--- Datasource.go
+++ Datasource.go
@@ -1,14 +1,28 @@
 package main
 
+import (
+	"time"
+)
+
 type Datasource struct {
-	Name string
-	path string
+	Name   string
+	path   string
+	output chan PowerSample
+
+	static ImpulseCount
 }
 
-func NewDatasource(name string) Datasource {
-	return Datasource{name, ""}
+func NewDatasource(name string, output chan PowerSample) *Datasource {
+	result := Datasource{name, "", output, 0.0}
+	return &result
 }
 
 func (self *Datasource) Init(name string) {
 	self.Name = name
+}
+
+func (self *Datasource) Poll() {
+	//println("Poll out")
+	self.output <- PowerSample{time.Now(), self.static}
+	self.static++
 }
--- Server.go
+++ Server.go
@@ -2,54 +2,80 @@ package main
 
 import (
 	"net/http"
-	"fmt"
+	//	"fmt"
+	"flag"
 	"log"
 	"time"
 )
 
-func main() {
-	println("Running server")
-	d := NewDatasource("Datei")
-	d.Init("Port")
-	println(d.Name)
-	s := PowerSample{time.Now(), 0, 0}
-	println(s.Time.String())
-
-	go poll()
-
-	serve()
+func defaultTimeFactor() time.Duration {
+	// Use time.Duration to get rid of casting
+	return 8
 }
 
-func serve() {
-	http.HandleFunc("/KWh", handlerKWh)
-	http.HandleFunc("/Watt", handlerWatt)
+func main() {
+	println("Running server")
+	input := make(chan PowerSample)
+	testDatasource := NewDatasource("Datei", input)
+	testDatasource.Init("Port")
+
+	sources := make([]*Datasource, 1)
+	sources[0] = testDatasource
+
+	var server Server
+	server.init()
+
+	flag.Parse()
+	basepath := flag.Arg(0)
+	println("Basepath is " + basepath)
+
+	provider := NewDataProvider("Solar", basepath, input)
+	server.Providers["Solar"] = provider
+	provider.pushSample(PowerSample{time.Now(), 2})
+	println(server.Providers["Solar"].currentValue())
+
+	go poll(sources)
+	server.serve()
+}
+
+func poll(sources []*Datasource) {
+	for {
+		//println(time.Now().String())
+		time.Sleep(1 * time.Second)
+		for _, current := range sources {
+			current.Poll()
+		}
+	}
+}
+
+type Server struct {
+	Providers map[string]*DataProvider
+}
+
+func (server *Server) serve() {
+	for key, provider := range server.Providers {
+		http.HandleFunc("/"+key+"/KWh", provider.handlerKWh)
+		http.HandleFunc("/"+key+"/Watt", provider.handlerWatt)
+		go provider.listen()
+	}
 	err := http.ListenAndServe(":8088", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-func poll() {
-	for {
-		println(time.Now().String())
-		time.Sleep(3 * time.Second)
-	}
+func (self *Server) init() {
+	self.Providers = make(map[string]*DataProvider)
 }
 
-func handlerKWh(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%f", 3.1415926)
-}
+type ImpulseCount int64
 
-func handlerWatt(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%f", 2.79)
-}
+type KWh float64
 
 type PowerSample struct {
 	time.Time
-	Watts		float64
-	KWh			float64
+	Impulses ImpulseCount
 }
-
 
 /*
 	Aquiring data
@@ -57,9 +83,24 @@ type PowerSample struct {
 	2. Data (value, timestamp) is pushed out of Datasource (chan?)
 	3. Delta of data is logged to simple file
 
-	File formate
-	Each byte is one impulse, content depends on time (1 = 1/8s?). Byte 255 means no impulse at all,
-	zero means that we've got more than one event in that timeframe
+	Raw file format
+	Each word is one impulse, content depends on time (1 = 1/32s?). Zero means that we've got more than one event in
+	that timeframe.
+	Filename equals order of file in week (timestamp of creation?) plus time factor.
+	Each file should contain exactly 1000 entries - thus allowing good guesses depending on count:
+	One file is one KWh, one word is one Wh.
+
+	Folders and cache files
+	Raw files aren't effective - they just save memory.
+	In addition, CVS files with timestamp and impulse count are used as "catalog".
+	A new folder is created when a week begins. This folder contains all raw files.
+
+	Solar/Year2014/Week01.csv
+	Solar/Year2014/Week01/4234343334@1:32.raw
+
+	Assuming a power consumption of 500W, we create 12 files per day, 84 per week.
+	This should be sufficient for a month-overview.
+	For a full day (24h), 720 samples should be enough (sample every two minutes -> keep one of 16 samples in average)
 
 	Load state once at startup, create Samples for current (and last) hour, day, week, month, year and total
 	Listen to sources, calculate new values (power consumed, watts) for each timestamp
@@ -69,26 +110,4 @@ type PowerSample struct {
 	Current values for all Datasources
 	Generate datasets on the fly (and cache them)
 	Create graphs from datasets
- */
-
-type DataProvider struct {
-	Name		string
-	samples		[]PowerSample
-}
-
-func (self *DataProvider) currentValue() float64 {
-	return 0.0
-}
-
-func (self *DataProvider) currentTendency() float64 {
-	return 0.0
-}
-
-func (self *DataProvider) registerSample(sample PowerSample) {
-	self.samples.append(sample)
-}
-
-func (self *DataProvider) valuesInTimeframe(start, end time.Timer) []PowerSample {
-	return self.samples
-}
-
+*/
